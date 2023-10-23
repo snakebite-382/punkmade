1b00d0a1df233f3859bbf4a4fb2c766a5495fb83
Caelin Limerick <caelouwho@gmail.com>
2023-10-23 15:34:55 -0400

    remove todo from readme

--- backend/frontend/tailwind.config.js
+++ backend/frontend/tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    colors: {
-      'grey.bg': "#292929"
-    },
-    extend: {},
+      colors: {
+          'grey-bg': "#1f1f1f",
+          'red-accent': "#800000",
+          'green-text': "#51e800",
+          'white-text': "#dedede",
+          'red-toast': "#c22f0e",
+          'orange-toast': "#e08607",
+          'yellow-toast': "#e0ce07",
+          'green-toast': "#2a9403",
+      },
+      extend: {},
   },
   plugins: [],
+  prefix: 'tw-'
 }
-
