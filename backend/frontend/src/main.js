e77705c3beb3225c284d1e7b5e22b06a0d91f752
Caelin Limerick <caelouwho@gmail.com>
2023-11-06 20:57:05 -0500

    remove sentry because it's huge and unnecessary

--- backend/frontend/src/main.js
+++ backend/frontend/src/main.js
@@ -5,9 +5,6 @@ import { createPinia } from 'pinia'
 import { createAuth0 } from '@auth0/auth0-vue';
 
 import VueFeather from 'vue-feather';
-
-import * as Sentry from '@sentry/vue'
-
 import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 import App from './App.vue'
@@ -39,30 +36,6 @@ app.use(createAuth0({
     audience: "https://punkmade.us.auth0.com/api/v2/"
   }
 }))
-
-Sentry.init({
-  app,
-  dsn: "https://5f8838e1f678677e4db91fc08d0fb1d8@o466290.ingest.sentry.io/4506175687950336",
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-    }),
-    new Sentry.Replay(),
-  ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
 
 app.use(router)
 
