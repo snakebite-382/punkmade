391501186bd44c88451dbb895fb562dab6e6ce72
Caelin Limerick <caelouwho@gmail.com>
2023-11-10 23:46:40 -0500

    Better splashscreen

--- backend/frontend/src/components/SplashScreen.vue
+++ backend/frontend/src/components/SplashScreen.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Welcome</h1>
+    <div id="Splash">
+        <h1 class="underline tw-text-xl">Welcome to PunkMade Alpha 1.0!</h1>
+        <h2 class="tw-text-lg">This project is still in very early development, all bugs can be submitted through github issue at <a class="tw-text-green" target="blank" href="https://github.com/snakebite-382/punkmade">https://github.com/snakebite-382/punkmade</a>. Press Login to get started!</h2> 
+    </div> 
 </template>
 
 <script>
